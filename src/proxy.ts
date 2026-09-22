e803ca4391a6077d4ee58ab9d41e9fa3365f6c11
happy-quokka98 <307491385+happy-quokka98@users.noreply.github.com>
2026-09-22 11:33:05 +0400

    fixed error in grades section and sorting

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,4 +1,4 @@
-ცimport { NextResponse } from 'next/server'
+import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 const allowedOrigins = ['http://localhost:3001', 'http://localhost:3000']
